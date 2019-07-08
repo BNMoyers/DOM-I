7b8c1f942df56ff103cb4b9f64e50cafe793ae12
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-08 16:58:32 -0500

    started on timer; WIP onLoad function with selectors

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    let msHun = 00;
+    let msTen = 00;
+    let secOne = 00;
+    let secTen = 00;
+
+    const linkMsHun = document.getElementById('msHundreds');
+    const linkMsTen = document.getElementById('msTens');
+    const linkSecOne = document.getElementById('secondOnes');
+    const linkSecTen = document.getElementById('secondTens');
+}
